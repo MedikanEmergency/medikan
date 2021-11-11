e8da16b1bb5d09c409c4ba3edcb0be95cf737888
hoang-cs <hoangproyume@gmail.com>
2021-11-11 21:52:21 +0700

    create UI and timer in S.O.S screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -5,6 +5,7 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:medikan/screens/chat%20screen/doctor_screen.dart';
 import 'package:medikan/screens/signup_screen.dart';
+import 'package:medikan/screens/sos_screen.dart';
 import 'package:medikan/themes/theme_data.dart';
 import 'package:medikan/components/sos_button.dart';
 import 'package:medikan/icons.dart';
