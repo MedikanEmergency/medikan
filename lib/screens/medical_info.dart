118416aa74538dd89d7c82ff5b8a8ba4efae3048
hongphucvo <vohongphuc2001@gmail.com>
2021-11-12 21:57:08 +0700

    work on grid view and add family

--- lib/screens/medical_info.dart
+++ lib/screens/medical_info.dart
@@ -1,253 +1,240 @@
-
-
-import 'dart:html';
+import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:medikan/themes/theme_data.dart';
 import 'package:medikan/icons.dart';
+import './personal_info.dart';
 
-class MedicalInfo extends StatelessWidget {
+class ListState {
+  String ill;
+  String level;
+  ListState(this.ill, this.level);
+}
+
+class MedicalInfo extends StatefulWidget {
+  bool edit;
+  MedicalInfo({Key? key, this.edit = false}) : super(key: key);
+
+  @override
+  _MedicalInfoState createState() => _MedicalInfoState();
+}
+
+class _MedicalInfoState extends State<MedicalInfo> {
+  // @override
+  // State<StatefulWidget> createState() {
+  //   // TODO: implement createState
+  //   throw UnimplementedError();
+  // }
+
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    final user_img =
+        "https://wallup.net/wp-content/uploads/2017/11/23/438674-duck-yellow.jpg";
+    List<ListState> user_ill = [
+      ListState("Huyết áp", "Mức nhẹ"),
+      ListState("Huyết áp", "Mức nhẹ")
+    ];
     return Scaffold(
-      
-      body: Column(
-        children: [
-          Row(children: [
-            ElevatedButton.icon( onPressed: (){},icon: Icon(Icons.reset_tv_rounded), label: Text("Quay về")),
-            Expanded(child:Column(
+      body: SafeArea(
+        child: Column(
+          children: [
+            //TODO: hốt của thương
+            Container(
+              alignment: Alignment.topLeft,
+              child: ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => PersonalInfo()));
+                  },
+                  icon: Icon(Icons.reset_tv_rounded),
+                  label: Text("Quay về")),
+            ),
+            Center(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                child: Column(children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      user_img,
+                    ),
+                    radius: 50.0,
+                  ),
+                  Text(
+                    "Võ Hồng Phúc",
+                    style: FontStyleData.H3_bold_36,
+                  ),
+                  Text("0919813156", style: FontStyleData.Subtitle_light_24),
+                ]),
+              ),
+            ),
+
+            Row(
               children: [
-                CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            "https://www.trendrr.net/wp-content/uploads/2017/06/Deepika-Padukone-1.jpg",
-                          ),
-                          
-                          radius: 50.0,
-                          
-                        ),
-                Text("Võ Hồng Phúc"),
-                Text("0919813156"),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Icon(
+                    Icons.add_alert_outlined,
+                    color: ColorData.sos,
+                    size: 36,
+                  ),
+                ),
+                Expanded(
+                  flex: 8,
+                  child: Padding(
+                      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: Column(
+                          children: user_ill
+                              .map((e) => Ill(
+                                  selectedIll: e.ill,
+                                  selectedLevel: e.level,
+                                  edit: widget.edit))
+                              .toList())),
+                ),
               ],
-            )
-            
-            )
-            ]
-          ),
-          GridView.count(
-          // Create a grid with 2 columns. If you change the scrollDirection to
-          // horizontal, this produces 2 rows.
-          crossAxisCount: 2,
-          // Generate 100 widgets that display their index in the List.
-          children: [
-            Container(
-              child: Column(
-                children: [
-                  Text("Chiều cao: (m)"),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      enabled: true,
-                    )
-                  )
-                ],
-              ),
-            ),
-            Container(
-              child: Column(
-                children: [
-                  Text("Chiều cao: (m)"),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      enabled: true,
-                      suffixText: "1m56",
-                    )
-                  )
-                ],
-              ),
-            ),
-            Container(
-              child: Column(
-                children: [
-                  Text("Cân nặng: (kg)"),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      enabled: true,
-                      suffixText: "1m56",
-                    )
-                  )
-                ],
-              ),
-            ),
-            Container(
-              child: Column(
-                children: [
-                  Text("Huyết áp: (mmHg)"),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      enabled: true,
-                    )
-                  )
-                ],
-              ),
-            ),
-            Container(
-              child: Column(
-                children: [
-                  Text("Nhịp tim"),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      enabled: true,
-                    )
-                  )
-                ],
-              ),
-            ),
-            Container(
-              child: Column(
-                children: [
-                  Text("Nhóm máu:"),
-                  DropdownButton<String>(
-      // value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? newValue) {
-        // setState(() {
-        //   dropdownValue = newValue!;
-        // });
-      },
-      items: <String>['One', 'Two', 'Free', 'Four']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    ),
-    DropdownButton<String>(
-      // value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? newValue) {
-        // setState(() {
-        //   dropdownValue = newValue!;
-        // });
-      },
-      items: <String>['+','-']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    ),
-            Container(
-              child: Column(
-                children: [
-                  Text("Tiền sử dị ứng:"),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      enabled: true,
-                    )
-                  )
-                ],
-              ),
-            ),
-                ],
-              ),
             ),
 
-          ]
-          
-          
-          ),
-
-        Row(
-          children: [
-            Icon(Icons.add_alert_outlined),
-            Expanded(
-              child: 
-              Column(
-                children: [
-                  DropdownButton<String>(
-      // value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? newValue) {
-        // setState(() {
-        //   dropdownValue = newValue!;
-        // });
-      },
-      items: <String>['One', 'Two', 'Free', 'Four']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    ),
-    DropdownButton<String>(
-      // value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? newValue) {
-        // setState(() {
-        //   dropdownValue = newValue!;
-        // });
-      },
-      items: <String>['One', 'Two', 'Free', 'Four']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    ),
-                ],
-              )
-              
-              
+            GridView.count(
+              // Create a grid with 2 columns. If you change the scrollDirection to
+              // horizontal, this produces 2 rows.
+              crossAxisCount: 2,
+              // Generate 100 widgets that display their index in the List.
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      Text("Chiều cao: (m)"),
+                      TextFormField(
+                          decoration: InputDecoration(
+                        enabled: true,
+                      ))
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      Text("Chiều cao: (m)"),
+                      TextFormField(
+                          decoration: InputDecoration(
+                        enabled: true,
+                        suffixText: "1m56",
+                      ))
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
-
-        ) 
-        ],
-
+        ),
       ),
     );
   }
 }
 
-class Item {
-  Item(
-    {
-    required this.expandedValue,
-    required this.headerValue,
-  });
+class Ill extends StatefulWidget {
+  String? selectedIll;
+  String? selectedLevel;
+  bool edit = false;
 
-  String expandedValue;
-  String headerValue;
+  Ill({Key? key, this.selectedIll, this.selectedLevel, required this.edit})
+      : super(key: key);
+
+  @override
+  IllState createState() => IllState();
 }
 
+class IllState extends State<Ill> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Container(
+          decoration: BoxDecoration(
+            // boxShadow: ,
+            border: Border.all(color: Colors.black, width: 2),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("Tên bệnh lý:"),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: DropdownButton<String>(
+                      value: widget.selectedIll,
+                      icon: const Icon(Icons.arrow_downward),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.deepPurple),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      onChanged: !widget.edit
+                          ? null
+                          : (String? newValue) {
+                              setState(() {
+                                widget.selectedIll = newValue!;
+                              });
+                            },
+                      items: <String>[
+                        'Béo phì',
+                        'Huyết áp',
+                        'Tiểu đường',
+                        'Tim mạch'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("Tình trạng:"),
+                  DropdownButton<String>(
+                    icon: const Icon(Icons.arrow_downward),
+                    iconSize: 24,
+                    elevation: 16,
+                    style: const TextStyle(color: Colors.deepPurple),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    value: widget.selectedLevel,
+                    items: <String>[
+                      'Mức nhẹ',
+                      'Mức trung bình',
+                      'Mức nguy hiểm'
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: !widget.edit
+                        ? null
+                        : (String? newValue) {
+                            setState(() {
+                              widget.selectedLevel = newValue!;
+                            });
+                          },
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/personal_info.dart
+++ lib/screens/personal_info.dart
@@ -1,33 +1,42 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:medikan/screens/medical_info.dart';
 import 'package:medikan/themes/theme_data.dart';
 import 'package:medikan/icons.dart';
+import './medical_info.dart';
 
 class PersonalInfo extends StatelessWidget {
+  // @override
+  // State<StatefulWidget> createState() {
+  //   // TODO: implement createState
+
+  //   throw UnimplementedError();
+  // }
+
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    final user_img =
+        "https://wallup.net/wp-content/uploads/2017/11/23/438674-duck-yellow.jpg";
     return Scaffold(
       appBar: AppBar(
         backgroundColor: ColorData.secondary,
         actions: [
           PopupMenuButton(
-            icon: Icon(
+              icon: Icon(
                 MyFlutterApp.setting,
                 color: ColorData.onPrimary,
               ),
-                itemBuilder: (context) => [
-                  PopupMenuItem(
-                    child: Text("First"),
-                    value: 1,
-                  ),
-                  PopupMenuItem(
-                    child: Text("Second"),
-                    value: 2,
-                  )
-                ]
-            )
-          
+              itemBuilder: (context) => [
+                    PopupMenuItem(
+                      child: Text("First"),
+                      value: 1,
+                    ),
+                    PopupMenuItem(
+                      child: Text("Second"),
+                      value: 2,
+                    )
+                  ])
         ],
         title: Text(
           "Tài khoản của bạn",
@@ -36,165 +45,163 @@ class PersonalInfo extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text("Personal Infor"),
-          Card(
-            child:Container(
-              height: 100,
-              color: Colors.white,
-              child: Row(
-                children: [
-                  Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Expanded(
-                        child:
-                        CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            "https://www.trendrr.net/wp-content/uploads/2017/06/Deepika-Padukone-1.jpg",
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: ColorData.primary,
+                borderRadius: new BorderRadius.circular(20.0),
+              ),
+              child: Container(
+                height: 100,
+                child: Row(
+                  children: [
+                    Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Expanded(
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              user_img,
+                            ),
+                            radius: 50.0,
                           ),
-                          radius: 50.0,
+                          flex: 2,
                         ),
-                        flex:2 ,
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child:Container(
-                      alignment: Alignment.topLeft,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            flex: 5,
-                            child: ListTile(
-                              title: Text("Shape Of You"),
-                              subtitle: Text("Ed Sheeran"),
-                            ),
+                    Expanded(
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        child: ListTile(
+                          title: Text(
+                            "Võ Hồng Phúc",
                           ),
-                          Expanded(
-                            flex: 5,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                TextButton(
-                                  child:Text("PLAY"),
-                                  onPressed: ()
-                                  {},
-                                ),
-                                SizedBox(width: 8,),
-                                TextButton(
-                                  child: Text("ADD TO QUEUE"),
-                                  onPressed: (){},
-                                ),
-                                SizedBox(width: 8,)
-                              ],
+                          subtitle: Text("0919813176"),
+                        ),
+                        decoration: BoxDecoration(
+                            // boxShadow: BoxShadow,
+                            //TODO: đổ bóng
                             ),
-                          )
-                        ],
                       ),
+                      flex: 8,
                     ),
-                    flex:8 ,
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-            elevation: 8,
-            margin: EdgeInsets.all(10),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => MedicalInfo()));
+              },
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  width: width * 0.3,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text("Xem hồ sơ"),
+                        Container(
+                          child: Icon(
+                            MyFlutterApp.goto,
+                          ),
+                        ),
+                      ]),
+                  decoration: BoxDecoration(
+                    borderRadius: new BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Danh sách người thân",
+                textAlign: TextAlign.left,
+                style: FontStyleData.Title_Bold_20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: new BorderRadius.circular(20.0),
+              ),
+              child: Container(
+                height: 100,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: ClipRRect(
+                          child: Image.network(
+                            user_img,
+                            fit: BoxFit.fill,
+                            width: width * 0.2,
+                            height: width * 0.2,
+                          ),
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.centerLeft,
+                          child: ListTile(
+                            title: Text("Võ Văn A (Cha)"),
+                            subtitle: Text("0913456123"),
+                          ),
+                        ),
+                        flex: 7,
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.delete,
+                            color: Colors.black,
+                            size: 36.0,
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+              // elevation: 8,
+              margin: EdgeInsets.all(10),
+            ),
           ),
           ElevatedButton.icon(
-
-            label: Text('Xem hồ sơ'),
-            onPressed: () {
-              print('Pressed');
-              //Move to Account info
+            label: Text(
+              "Thêm người thân",
+              style: FontStyleData.Title_Bold_20,
+            ),
+            onPressed: () => {
+              //do something
             },
             icon: Icon(
-            Icons.favorite,
-            color: Colors.pink,
-            size: 24.0,
-          ),
+              Icons.add,
+              color: Colors.black,
+              size: 24.0,
+            ),
             style: ElevatedButton.styleFrom(
+              primary: Colors.white,
               shape: new RoundedRectangleBorder(
                 borderRadius: new BorderRadius.circular(20.0),
               ),
             ),
           ),
-          Text("Danh sách người thân"),
-          Card(
-            child:Container(
-
-                height: 100,
-                color: Colors.white,
-
-              child: Row(
-                children: [
-                  Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Expanded(
-                        child:
-                        CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            "https://www.trendrr.net/wp-content/uploads/2017/06/Deepika-Padukone-1.jpg",
-                          ),
-                          radius: 50.0,
-                        ),
-                        flex:2 ,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child:Container(
-                      alignment: Alignment.topLeft,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: ListTile(
-                              title: Text("Shape Of You"),
-                              subtitle: Text("Ed Sheeran"),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    flex:7 ,
-                  ),
-                Expanded(
-                  child:
-                    ElevatedButton.icon(
-                      label:Text(""),
-                      onPressed: () => {
-                        //do something
-                      },
-                      icon: Icon(
-                        Icons.favorite,
-                        color: Colors.pink,
-                        size: 24.0,
-                      ),
-                    ),
-                    flex:1,)
-                ],
-              ),
-            ),
-            elevation: 8,
-            margin: EdgeInsets.all(10),
-          ),
-          ElevatedButton.icon(
-            label:Text(""),
-            onPressed: () => {
-              //do something
-            },
-            icon: Icon(
-              Icons.favorite,
-              color: Colors.pink,
-              size: 24.0,
-            ),
-          ),
-
         ],
-
       ),
     );
   }
 }
-
